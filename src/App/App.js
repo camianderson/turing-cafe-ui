b660c234890089e57312547123c53ea6dbf31dc1
Cami Anderson <camidantas@icloud.com>
2022-07-20 12:19:14 -0400

    Add basic stucture for the app

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import List from '../List/List';
+import Form from '../Form/Form';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form />
         </div>
         <div className='resy-container'>
-          
+          <List />
         </div>
       </div>
     )
